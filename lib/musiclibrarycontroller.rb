1bc87ce13bb55d0622d42ed342d7027c6165693d
Elana Ryznar <eryzna@gmail.com>
2018-08-22 23:31:32 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -30,7 +30,9 @@ class MusicLibraryController
         list_genres
       elsif input=="list artist"
         list_songs_by_artist
-        
+      elsif input =="list genre"
+        list_songs_by_genre
+      end
     end
 
 
