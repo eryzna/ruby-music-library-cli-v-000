b88dcee582802fcaa4095a1d6a3fdbb8e5b0f863
Elana Ryznar <eryzna@gmail.com>
2018-08-22 23:16:10 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -24,7 +24,21 @@ class MusicLibraryController
     end
 
     def list_songs
-    end
+   Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+     puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+   end
+ end
+
+ def list_artists
+   Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
+     puts "#{i}. #{a.name}"
+   end
+ end
+
+ def list_genres
+   Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+     puts "#{i}. #{g.name}"
+   end
 
 
   end
