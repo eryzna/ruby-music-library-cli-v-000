f2532fc88d3e4ca6fdc74cfffbd6756c6c4ef5b2
Elana Ryznar <eryzna@gmail.com>
2018-08-22 23:25:30 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -23,6 +23,24 @@ class MusicLibraryController
       input=gets.chomp
     end
 
+    case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
+    end
+  end
+
+
     def list_songs
       Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
         puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
@@ -74,21 +92,5 @@ class MusicLibraryController
 
       puts "Playing #{song.name} by #{song.artist.name}" if song
     end
-  end
-end
-
-case input
-  when "list songs"
-    list_songs
-  when "list artists"
-    list_artists
-  when "list genres"
-    list_genres
-  when "list artist"
-    list_songs_by_artist
-  when "list genre"
-    list_songs_by_genre
-  when "play song"
-    play_song
   end
 end
